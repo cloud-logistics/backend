2286f3ce3bac2b7ee0f9dd563df56301bb1c7c7e
于春深 <chsh.yu@hnair.com>
2017-05-22 09:50:53 +0800

    根据数据结构变动，修改解析模块

--- sensor/db.py
+++ sensor/db.py
@@ -19,19 +19,24 @@ def save_to_db(msg):
         maxconn=20,
         database="cloudbox",
         user="postgres",
-        password="root",
+        password="postgres",
         host="127.0.0.1",
         port="5432"
     )
     conn = pool.getconn()
     cur = conn.cursor()
-    sql = 'insert into iot.sensor_data(timestamp, clientid, temperature, humidity, latitude, longitude) ' \
+    sql = 'insert into iot.sensor_data(timestamp, deviceid, temperature, humidity, latitude, longitude, ' \
+          'speed, collide, light, legacy) ' \
           'values (\'' + str(data['timestamp']) + '\',\'' + \
-          str(data['clientid']) + '\',\'' +\
+          str(data['deviceid']) + '\',\'' +\
           str(data['temperature']) + '\',\'' + \
           str(data['humidity']) + '\',\'' + \
           str(data['latitude']) + '\',\'' + \
-          str(data['longitude']) + '\')'
+          str(data['longitude']) + '\',\'' + \
+          str(data['speed']) + '\',\'' + \
+          str(data['collide']) + '\',\'' + \
+          str(data['light']) + '\',\'' + \
+          str(data['legacy']) + '\')'
     log.debug('sql: ' + sql)
     cur.execute(sql)
     cur.close()
@@ -40,30 +45,21 @@ def save_to_db(msg):
 
 
 def parse(msg):
-    s = json.loads(str(msg))
-    data = s['datastreams']
-    timestamp = ''
-    clientid = ''
-    temperature = ''
-    humidity = ''
-    latitude = ''
-    longitude = ''
+    body = json.loads(str(msg))
+    s = body['data']
+    timestamp = s['utc']
+    deviceid = s['device_id']
+    temperature = s['temp']
+    humidity = s['humi']
+    latitude = s['latitude']
+    longitude = s['longitude']
+    speed = s['speed']
+    collide = s['collide']
+    light = s['light']
+    legacy = s['legacy']
 
-    for element in data:
-        if element.has_key('Time_UTC'):
-            timestamp = element['Time_UTC']
-
-        if element.has_key('ClientId'):
-            clientid = element['ClientId']
-
-        if element.has_key('id') and element['id'] == 'TempHumi':
-            temperature = element['datapoints'][0]['Temperature']
-            humidity = element['datapoints'][0]['Humidity']
-
-        if element.has_key('id') and element['id'] == 'GPS':
-            latitude = element['datapoints'][0]['Latitude']
-            longitude = element['datapoints'][0]['Longitude']
-
-    return {'timestamp': timestamp, 'clientid': clientid,
+    return {'timestamp': timestamp, 'deviceid': deviceid,
             'temperature': temperature, 'humidity': humidity,
-            'latitude': latitude, 'longitude': longitude}
+            'latitude': latitude, 'longitude': longitude,
+            'speed': speed, 'collide': collide, 'light': light,
+            'legacy': legacy}
