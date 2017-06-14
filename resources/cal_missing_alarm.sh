f5f6d1ca21ffc70d675c077bf5195a5300708410
于春深 <chsh.yu@hnair.com>
2017-06-14 16:30:34 +0800

    修改定时执行数据库函数脚本错误问题

--- resources/cal_missing_alarm.sh
+++ resources/cal_missing_alarm.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-psql -U postgres -d cloudbox -h 127.0.0.1 -c "select iot.cal_missing_alarm()"
+PSQL='/usr/local/postgresql/bin/psql'
+HOST='127.0.0.1'
+USER='postgres'
+DB='cloudbox'
+
+date "+%Y-%m-%d %H:%M:%S" >> /opt/cloudbox/runlog.log 2>&1
+COMMAND="SELECT iot.cal_missing_alarm()"
+${PSQL} -U ${USER} -d ${DB} -h ${HOST}  -c "${COMMAND}" >> /opt/cloudbox/runlog.log 2>&1
