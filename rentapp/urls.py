b594339c890ad73a1622194295d1fb02489c43c7
luocheng <cheng.luo@hnair.com>
2017-10-30 16:51:34 +0800

    delete get dispatch

--- rentapp/urls.py
+++ rentapp/urls.py
@@ -22,10 +22,9 @@ urlpatterns = [
 
     url(r'^site$', views.add_site),                                # 新增堆场
     url(r'^site/(?P<id>\d+)/', views.delete_site),                 # 删除堆场
-    url(r'^site/(?P<id>\d+)', views.modify_site),                 # 修改堆场
-    url(r'^sites$', views.get_sites),                               # 获取堆场
+    url(r'^site/(?P<id>\d+)', views.modify_site),                  # 修改堆场
+    url(r'^sites$', views.get_sites),                              # 获取堆场
 
     url(r'^siteStream/(?P<id>\d+)/', views.get_site_stream),       # 查询堆场进出箱子流水
     url(r'^boxinout$', views.box_inout),                           # 箱子进出仓库接口
-    url(r'^dispatch$', views.get_dispatch),                        # 获取云箱调度策略
 ]
