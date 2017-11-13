d9d925ee1eea911a2bec9c3bad8ddc706927f4be
guankai <Rootroot1>
2017-11-13 17:24:19 +0800

    add site statistics job

--- cloudbox/celery.py
+++ cloudbox/celery.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
@@ -24,6 +26,7 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(minute='*/15', hour='*'), billing.s())
     sender.add_periodic_task(crontab(minute='*/15', hour='*'), cancel_appointment.s())
+    sender.add_periodic_task(crontab(minute=1, hour=0), generate_site_stat.s())
 
 
 @app.task
@@ -70,6 +73,50 @@ def cancel_appointment():
         appointment.flag = 2
         appointment.save()
     log.info("cancel appointment end....")
+
+
+@app.task
+def generate_site_stat():
+    from monservice.models import SiteInfo
+    from rentservice.models import RentLeaseInfo
+    from rentservice.models import SiteStat
+    from rentservice.models import SiteStatDetail
+    from monservice.models import BoxTypeInfo
+    from rentservice.utils import logger
+    import uuid
+    import datetime
+    import pytz
+    tz = pytz.timezone(settings.TIME_ZONE)
+    log = logger.get_logger(__name__)
+    log.info("statistic site box start...")
+    # 轮询所有的仓库信息
+    site_list = SiteInfo.objects.all()
+    box_types = BoxTypeInfo.objects.all()
+    now = datetime.datetime.now(tz=tz)
+    start_time = now + datetime.timedelta(days=-1)
+    stat_day = start_time.strftime('%Y-%m-%d')
+    for site in site_list:
+        # 获取所有转出的箱子信息 上一日的所有数据
+        box_out = RentLeaseInfo.objects.filter(off_site=site, lease_start_time__gte=start_time)
+        # 获取所有入场的箱子信息 上一日的所有数据
+        box_in = RentLeaseInfo.objects.filter(on_site=site, lease_end_time__gte=start_time)
+        # 统计总数
+        stat_id = str(uuid.uuid1())
+        site_stat = SiteStat(stat_id=stat_id, stat_day=stat_day, total_in=box_in.count(), total_out=box_out.count(),
+                             site=site)
+        site_stat.save()
+        # 获取不同类型箱子的in & out
+        box_type_stat = []
+        for box_type in box_types:
+            # box out count
+            out_count = box_out.filter(box__type=box_type).count()
+            # box in count
+            in_count = box_in.filter(box__type=box_type).count()
+            stat_detail = SiteStatDetail(detail_id=str(uuid.uuid1()), box_type=box_type, box_out=out_count,
+                                         box_in=in_count, sitestat=site_stat)
+            box_type_stat.append(stat_detail)
+        SiteStatDetail.objects.bulk_create(box_type_stat)
+    log.info('statistic site box end....')
 
 
 @app.task(bind=True)
