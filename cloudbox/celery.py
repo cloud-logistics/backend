93f30f884181a46fc0d7a0cd4d221b089704081a
mark <ke.li1@hnair.com>
2017-12-15 14:57:27 +0800

    add auth redis update celery task

--- cloudbox/celery.py
+++ cloudbox/celery.py
@@ -29,6 +29,7 @@ def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(crontab(minute=1, hour=0), generate_site_stat.s())
     # sender.add_periodic_task(crontab(minute=15, hour=0), box_rent_fee_daily_billing.s())
     sender.add_periodic_task(crontab(minute=0, hour=1), box_rent_fee_month_billing.s())
+    sender.add_periodic_task(crontab(minute='*/5', hour='*'), update_redis_auth_info.s())
 
 
 @app.task
@@ -255,6 +256,45 @@ def box_rent_fee_month_billing():
     else:
         log.info("no BoxRentFeeDetail records. do nothing")
     log.info("BoxRentFeeByMonth billing end ...")
+
+
+@app.task
+def update_redis_auth_info():
+    from rentservice.models import AccessGroup, AccessUrlGroup, AuthUserGroup
+    from rentservice.serializers import AccessGroupSerializer, AuthUserGroupSerializer, AccessUrlGroupSerializer
+    from rentservice.utils import logger
+    from rentservice.utils.redistools import RedisTool
+    PERMISSION_GROUP_HASH = 'permissions_group_hash'
+    PERMISSION_URL_HASH = 'permissions_url_hash'
+    log = logger.get_logger(__name__)
+    log.info("update_redis_auth_info begin")
+    try:
+        redis_pool = RedisTool()
+        conn = redis_pool.get_connection()
+        access_group_ret = AccessGroup.objects.all()
+        access_group_list = AccessGroupSerializer(access_group_ret, many=True).data
+        groupid_group_dic = {}
+        for item in access_group_list:
+            groupid_group_dic[item['access_group_id']] = item['group']
+        ret = AuthUserGroup.objects.all()
+        auth_user_group = AuthUserGroupSerializer(ret, many=True).data
+        for item in auth_user_group:
+            conn.hset(PERMISSION_GROUP_HASH, item['user_token'], groupid_group_dic[item['group']])
+        for item_access_group in access_group_list:
+            ret = AccessUrlGroup.objects.filter(access_group__group=item_access_group['group'])
+            access_url_list = []
+            access_url_group = AccessUrlGroupSerializer(ret, many=True).data
+            for item in access_url_group:
+                access_url_list.append(item['access_url_set'])
+            if access_url_list:
+                final_hash_value = ','.join(access_url_list)
+            else:
+                final_hash_value = ''
+            if final_hash_value:
+                conn.hset(PERMISSION_URL_HASH, item_access_group['group'], final_hash_value)
+    except Exception, e:
+        log.error(repr(e))
+    log.info("update_redis_auth_info end")
 
 
 @app.task(bind=True)
