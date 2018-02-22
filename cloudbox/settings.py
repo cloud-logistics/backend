181278d93b98ea6742d7e5185afd0b0f06c15720
于春深 <chsh.yu@hnair.com>
2018-02-22 10:14:27 +0800

    modify db passwd

--- cloudbox/settings.py
+++ cloudbox/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         },
         'NAME': 'cloudbox',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': '@CloudBox601',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
