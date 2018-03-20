314e1a36b7849caaeaa921df1297de00d32a1c2c
luocheng <cheng.luo@hnair.com>
2018-03-20 16:05:59 +0800

    modify flow status

--- tms/urls.py
+++ tms/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
 
     url(r'^fishing$', flow.fishing),  # 捕捞
     url(r'^loadup$', flow.load_up),  # 装车
-    url(r'^loadoff/(?P<user_id>[0-9a-zA-Z-]+)$', flow.load_off),  # 装车
+    url(r'^loadoff$', flow.load_off),  # 装车
     url(r'^order$', flow.get_order),  # 获取订单ID
     url(r'^fisherylist$', flow.get_fishery_list),  # 获取渔场列表
     url(r'^fishtypelist$', flow.get_fishtype_list),  # 获取虾类型列表
