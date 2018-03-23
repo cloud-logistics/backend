02de3e5962b13914ae75f86960851c61d626477f
mark <ke.li1@hnair.com>
2018-03-23 15:56:40 +0800

    notify api add

--- tms/urls.py
+++ tms/urls.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 from tms.view import sensor, order, flow
-from tms.view import auth, user
+from tms.view import auth, user, notify
 
 urlpatterns = [
     url(r'^receive_data$', sensor.receive_data),                           # 传感器接收数据
@@ -32,5 +32,8 @@ urlpatterns = [
     url(r'^user/userdetail/(?P<user_id>[0-9a-zA-Z-]+)$', user.user_detail),  # user log out
     url(r'^user/adduser$', user.add_user),  # user log out
     url(r'^user/rmuser/(?P<user_id>[0-9a-zA-Z-]+)/$', user.del_user),  # user log out
+    url(r'^notify/list/(?P<user_id>[0-9a-zA-Z-]+)$', notify.get_notify_list_by_user),  # 获取用户所有通知
+    url(r'^notify/set', notify.set_notify_read_flag),  # 更新消息状态
+    url(r'^notify/delete/(?P<notify_id>[0-9]+)$', notify.delete_notify),  # 删除消息
 ]
 
